55716933d27a2e17ab57769b0d72f1ebcede3858
unknown <tingzhang0997@gmail.com>
2021-09-09 13:54:51 +1000

    test using React Hooks

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import ipadMini from './ipad-mini.jpg';
 import iconFile from './icons-file.svg';
@@ -7,103 +7,102 @@ import htmlContent from './content.json';
 
 import './App.css';
 
-class App extends React.Component {
-  constructor(props){
-    super(props);
-    this.state= {
-      htmlHeader:'',
-      contentTitle:'',
-      contentDescription:'',
-      contentArray:'',
-      nextPageLink:''
-    }
-  }
+function App (){
+  // constructor(props){
+  //   super(props);
+  //   this.state= {
+  //     htmlHeader:'',
+  //     contentTitle:'',
+  //     contentDescription:'',
+  //     contentArray:'',
+  //     nextPageLink:''
+  //   }
+  // }
+  const [htmlHeader,setHtmlHeader]=useState('');
+  const [contentDescription, setContentDescription]=useState('');
+  const [contentIndex,setContentIndex]=useState('');
+  const [nextPageLink,setNextPageLink]=useState('');
 
-  componentDidMount(){
-    const initialContentArray=0;
-    this.setState({
-      htmlHeader:htmlContent.title,
-      contentDescription:htmlContent.content[initialContentArray].description,
-      contentArray:initialContentArray,
-      nextPageLink:htmlContent.content[(initialContentArray+1)].title
-    })
-  }
+  // componentDidMount(){
+  //   const initialContentArray=0;
+  //   this.setState({
+  //     htmlHeader:htmlContent.title,
+  //     contentDescription:htmlContent.content[initialContentArray].description,
+  //     contentArray:initialContentArray,
+  //     nextPageLink:htmlContent.content[(initialContentArray+1)].title
+  //   })
+  // }
 
- handleClick=()=>{
-    const currentContentArray=this.state.contentArray+1;
-    const totalContentArray=htmlContent.content.length;
-    if(currentContentArray< (totalContentArray-1)){
-      this.setState({
-          contentDescription:htmlContent.content[currentContentArray].description,
-          contentArray:currentContentArray,
-          nextPageLink:htmlContent.content[(currentContentArray+1)].title
-
-    })
-    }else if (currentContentArray=== (totalContentArray-1)){
-      this.setState({
-        nextPageLink:'This is the last page.'
-      })
-    }else{
+  useEffect(()=>{
+        const initialContentIndex=0;
+        setHtmlHeader(htmlContent.title);
+        setContentDescription(htmlContent.content[initialContentIndex].description);
+        setContentIndex(initialContentIndex);
+        setNextPageLink(htmlContent.content[(initialContentIndex+1)].title)
+  },[])
+  
+ const handleClick=()=>{
+    const currentContentIndex=contentIndex+1;
+    const totalContentLength=htmlContent.content.length;
+    if (currentContentIndex< (totalContentLength-1)){
+          setContentDescription(htmlContent.content[currentContentIndex].description);
+          setContentIndex(currentContentIndex);
+          setNextPageLink(htmlContent.content[(currentContentIndex+1)].title);
+    } else if (currentContentIndex=== (totalContentLength-1)){
+        setNextPageLink('This is the last page');
+    } else{
       return
     }  
   }
 
-  handleClickBack=()=>{
-    const currentContentArray=this.state.contentArray-1;
-    if(currentContentArray>=0){
-      this.setState({
-          contentDescription:htmlContent.content[currentContentArray].description,
-          contentArray:currentContentArray,
-          nextPageLink:htmlContent.content[(currentContentArray+1)].title
-
-    })
+ const handleClickBack=()=>{
+    const currentContentIndex=contentIndex-1;
+    if(currentContentIndex>=0){
+          setContentDescription(htmlContent.content[currentContentIndex].description);
+          setContentIndex(currentContentIndex);
+          setNextPageLink(htmlContent.content[(currentContentIndex+1)].title);
     }else{
       return
     } 
   }
-
- 
-
-  render(){
     
-    return(
-      <div className="App">
-          <header className="App-header">
-          <div className='App-title'>
-            <div className='header-image' style={{backgroundImage:`url(${iconFile})`
-            }}
-            />
-
-            <div>{this.state.htmlHeader}</div>
-          </div>
-          
-             
-            <div className="arrow-up"></div>
-          </header>
-          <div className='App-main'>
-            <div className='App-image' style={{backgroundImage:`url(${ipadMini})`
+  return(
+    <div className="App">
+        <header className="App-header">
+        <div className='App-title'>
+          <div className='header-image' style={{backgroundImage:`url(${iconFile})`
           }}
           />
-            <section className='App-text'>
-            <div dangerouslySetInnerHTML={{__html:`${this.state.contentDescription}` }}/>
-            </section>
+
+          <div>{htmlHeader}</div>
+        </div>
+        
+            
+          <div className="arrow-up"></div>
+        </header>
+        <div className='App-main'>
+          <div className='App-image' style={{backgroundImage:`url(${ipadMini})`
+        }}
+        />
+          <section className='App-text'>
+          <div dangerouslySetInnerHTML={{__html:`${contentDescription}` }}/>
+          </section>
+        </div>
+        <footer className='App-footer'>
+          <div className='back-options' onClick={handleClickBack}>
+            <div className="arrow-left"></div>
+            {contentIndex?
+            'Back':
+            'This is the first page'}
           </div>
-          <footer className='App-footer'>
-            <div className='back-options' onClick={this.handleClickBack}>
-              <div className="arrow-left"></div>
-              {this.state.contentArray?
-              'Back':
-              ''}
-            </div>
-            <div className='previous-options' onClick={this.handleClick}>
-              {this.state.nextPageLink}
-              <div className="arrow-right"></div>
-            </div>
-          </footer>
-      
-      </div>
-    );
-  }
+          <div className='previous-options' onClick={handleClick}>
+            {nextPageLink}
+            <div className="arrow-right"></div>
+          </div>
+        </footer>
+    
+    </div>
+  );
 }
 
 export default App;

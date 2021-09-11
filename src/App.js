84afc2900f51af892c6ec666c4439171f9e411c1
unknown <tingzhang0997@gmail.com>
2021-09-11 14:27:41 +1000

    fix eslint issues

--- src/App.js
+++ src/App.js
@@ -1,88 +1,80 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
+import ipadMini from "./ipad-mini.jpg";
+import iconFile from "./icons-file.svg";
+import htmlContent from "./content.json";
+import "./App.css";
 
-import ipadMini from './ipad-mini.jpg';
-import iconFile from './icons-file.svg';
-import htmlContent from './content.json';
+function App() {
+  const [htmlHeader, setHtmlHeader] = useState("");
+  const [contentDescription, setContentDescription] = useState("");
+  const [contentIndex, setContentIndex] = useState("");
+  const [nextPageLink, setNextPageLink] = useState("");
 
+  useEffect(() => {
+    const initialContentIndex = 0;
+    setHtmlHeader(htmlContent.title);
+    setContentDescription(htmlContent.content[initialContentIndex].description);
+    setContentIndex(initialContentIndex);
+    setNextPageLink(htmlContent.content[initialContentIndex + 1].title);
+  }, []);
 
-import './App.css';
+  const handleClick = () => {
+    const currentContentIndex = contentIndex + 1;
+    const totalContentLength = htmlContent.content.length;
+    if (currentContentIndex < totalContentLength - 1) {
+      setContentDescription(
+        htmlContent.content[currentContentIndex].description
+      );
+      setContentIndex(currentContentIndex);
+      setNextPageLink(htmlContent.content[currentContentIndex + 1].title);
+    } else if (currentContentIndex === totalContentLength - 1) {
+      setNextPageLink("This is the last page");
+    }
+  };
 
-function App (){
-  
-  const [htmlHeader,setHtmlHeader]=useState('');
-  const [contentDescription, setContentDescription]=useState('');
-  const [contentIndex,setContentIndex]=useState('');
-  const [nextPageLink,setNextPageLink]=useState('');
+  const handleClickBack = () => {
+    const currentContentIndex = contentIndex - 1;
+    if (currentContentIndex >= 0) {
+      setContentDescription(
+        htmlContent.content[currentContentIndex].description
+      );
+      setContentIndex(currentContentIndex);
+      setNextPageLink(htmlContent.content[currentContentIndex + 1].title);
+    }
+  };
 
-
-  useEffect(()=>{
-        const initialContentIndex=0;
-        setHtmlHeader(htmlContent.title);
-        setContentDescription(htmlContent.content[initialContentIndex].description);
-        setContentIndex(initialContentIndex);
-        setNextPageLink(htmlContent.content[(initialContentIndex+1)].title)
-  },[])
-  
- const handleClick=()=>{
-    const currentContentIndex=contentIndex+1;
-    const totalContentLength=htmlContent.content.length;
-    if (currentContentIndex< (totalContentLength-1)){
-          setContentDescription(htmlContent.content[currentContentIndex].description);
-          setContentIndex(currentContentIndex);
-          setNextPageLink(htmlContent.content[(currentContentIndex+1)].title);
-    } else if (currentContentIndex=== (totalContentLength-1)){
-        setNextPageLink('This is the last page');
-    } else{
-      return
-    }  
-  }
-
- const handleClickBack=()=>{
-    const currentContentIndex=contentIndex-1;
-    if(currentContentIndex>=0){
-          setContentDescription(htmlContent.content[currentContentIndex].description);
-          setContentIndex(currentContentIndex);
-          setNextPageLink(htmlContent.content[(currentContentIndex+1)].title);
-    }else{
-      return
-    } 
-  }
-    
-  return(
+  return (
     <div className="App">
-        <header className="App-header">
-        <div className='App-title'>
-          <div className='header-image' style={{backgroundImage:`url(${iconFile})`
-          }}
+      <header className="App-header">
+        <div className="App-title">
+          <div
+            className="header-image"
+            style={{ backgroundImage: `url(${iconFile})` }}
           />
-
           <div>{htmlHeader}</div>
         </div>
-        
-            
-          <div className="arrow-up"></div>
-        </header>
-        <div className='App-main'>
-          <div className='App-image' style={{backgroundImage:`url(${ipadMini})`
-        }}
+        <div className="arrow-up"/>
+      </header>
+      <div className="App-main">
+        <div
+          className="App-image"
+          style={{ backgroundImage: `url(${ipadMini})` }}
         />
-          <section className='App-text'>
-          <div dangerouslySetInnerHTML={{__html:`${contentDescription}` }}/>
-          </section>
+        <section className="App-text">
+          <div dangerouslySetInnerHTML={{ __html: `${contentDescription}` }} />
+        </section>
+      </div>
+
+      <footer className="App-footer">
+        <div className="back-options" onClick={handleClickBack}>
+          <div className="arrow-left"/>
+          {contentIndex ? "Back" : "This is the first page"}
         </div>
-        <footer className='App-footer'>
-          <div className='back-options' onClick={handleClickBack}>
-            <div className="arrow-left"></div>
-            {contentIndex?
-            'Back':
-            'This is the first page'}
-          </div>
-          <div className='previous-options' onClick={handleClick}>
-            {nextPageLink}
-            <div className="arrow-right"></div>
-          </div>
-        </footer>
-    
+        <div className="previous-options" onClick={handleClick}>
+          {nextPageLink}
+          <div className="arrow-right"/>
+        </div>
+      </footer>
     </div>
   );
 }
